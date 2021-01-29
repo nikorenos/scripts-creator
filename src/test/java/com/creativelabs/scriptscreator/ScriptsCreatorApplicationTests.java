060d0f417546e8747e7c2f3b70798fd511a818cd
Sentarion <mbalabox@gmail.com>
2021-01-30 00:42:50 +0100

    project creation

---
+++ src/test/java/com/creativelabs/scriptscreator/ScriptsCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creativelabs.scriptscreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptsCreatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
